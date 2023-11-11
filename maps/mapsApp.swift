f87a25bd602970139a1ac8e199e856a81c6ea734
Makarov96 <estev97@gmail.com>
2023-11-11 12:40:32 -0600

    Initial Commit

---
+++ maps/mapsApp.swift
@@ -0,0 +1,17 @@
+//
+//  mapsApp.swift
+//  maps
+//
+//  Created by Guerin Steven Colocho Chacon on 11/11/23.
+//
+
+import SwiftUI
+
+@main
+struct mapsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
